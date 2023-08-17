6080a0e1a54fa0121d93db022591f2e16e0f58f6
Nakagawa Kosuke <kosukenakagawa730@gmail.com>
2023-08-18 05:23:32 +0900

    Initial Commit

---
+++ LazyVGridA&LazyHGrid/LazyVGridA_LazyHGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyVGridA_LazyHGridApp.swift
+//  LazyVGridA&LazyHGrid
+//
+//  Created by Nakagawa Kosuke on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct LazyVGridA_LazyHGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
